511409fec7c0e5073e5e6476471941dcf46b94e3
SwetyCore <swety2003@outlook.com>
2022-03-02 10:05:40 +0800

    First Upload

---
+++ MyWidget/Form1.cs
@@ -0,0 +1,204 @@
+﻿using Microsoft.Web.WebView2.Core;
+using Microsoft.Web.WebView2.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyWidget
+{
+
+    public partial class Form1 : Form
+    {
+
+
+        [DllImport("user32.dll")]
+        public static extern int GetAsyncKeyState(Keys vKey);
+
+
+        CoreWebView2 webView;
+        WebView2 webViewControl;
+
+        ResourceHandler resourceHandler;
+
+        InternalApi internalApi;
+        public static bool debug = false;
+        public static readonly string myhost="http://www.sc.mywidget.com";
+        public static string internalUrl = "http://www.sc.mywidget.com";
+
+
+
+        public static int count = 0;
+        const int target = 70;//长按0.7s
+#if DEBUG
+        string WebRoot = @"E:\HTML\vuewidgets\dist\";
+#else
+        string WebRoot = "";
+#endif
+        public Form1()
+        {
+            InitializeComponent();
+            int workWidth = Screen.PrimaryScreen.WorkingArea.Width;
+            int workHeight = Screen.PrimaryScreen.WorkingArea.Height;
+            Location = new Point(workWidth-600, 0);
+            Size = new Size(600, workHeight);
+            WebRoot = Path.Combine(Application.StartupPath, "WebRoot"); 
+            if (!File.Exists(Path.Combine(WebRoot,"index.html")))
+            {
+                MessageBox.Show("资源文件丢失，请重新下载/安装此程序！");
+                Environment.Exit(1);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.notifyIcon1.Visible = true;
+            webViewControl = new WebView2();
+            Task<CoreWebView2Environment> createEnvTask = CoreWebView2Environment.CreateAsync(userDataFolder: Path.GetFullPath("app\\data\\cefdata"));
+            createEnvTask.Wait();
+            CoreWebView2Environment env = createEnvTask.Result;
+            Controls.Add(webViewControl);
+            webViewControl.Dock = DockStyle.Fill;
+            webViewControl.EnsureCoreWebView2Async(env);
+            webViewControl.CoreWebView2InitializationCompleted += WebView2_CoreWebView2InitializationCompleted;
+
+        }
+        void initWebView2()
+        {
+            webView.Settings.IsPinchZoomEnabled = false;
+            webView.Settings.IsSwipeNavigationEnabled = false;
+            webView.AddWebResourceRequestedFilter("*", CoreWebView2WebResourceContext.All);
+            resourceHandler = new ResourceHandler(webView.Environment);
+            webView.WebResourceRequested += WebView_WebResourceRequested;
+            webView.NavigationStarting += NavigationStarting;
+
+
+
+            internalApi = new InternalApi(resourceHandler);
+
+        }
+        private void NavigationStarting(object sender, CoreWebView2NavigationStartingEventArgs e)
+        {
+            if (!e.Uri.StartsWith(internalUrl))
+            {
+                e.Cancel = true;
+                webView.Navigate(internalUrl);
+            }
+        }
+        private void WebView_WebResourceRequested(object sender, CoreWebView2WebResourceRequestedEventArgs e)
+        {
+            e.Response = handleRequest(e.Request);
+            e.GetDeferral().Complete();
+        }
+        public CoreWebView2WebResourceResponse handleRequest(CoreWebView2WebResourceRequest request)
+        {
+
+            if (request.Uri.StartsWith(internalUrl)){
+                string path = request.Uri.Substring(internalUrl.Length, request.Uri.Length - internalUrl.Length);
+                if (path.StartsWith("/"))
+                {
+                    path = path.Substring(1);
+                }
+                if (path == "" || path.EndsWith("/"))
+                {
+                    path += "index.html";
+                }
+                if (path.StartsWith("api/"))
+                {
+                    return internalApi.ApiReqHandler(request);
+                }
+                string mimetype = "";
+                if (debug)
+                {
+                    return resourceHandler.FromFilePath("", "", true);
+                }
+                else
+                {
+                    return resourceHandler.FromFilePath(Path.Combine(WebRoot, path), mimetype, true);
+                }
+
+            }
+
+
+            return resourceHandler.FromFilePath("", "", true);
+        }
+        private void WebView2_CoreWebView2InitializationCompleted(object sender, CoreWebView2InitializationCompletedEventArgs e)
+        {
+            if (e.IsSuccess)
+            {
+                webView = webViewControl.CoreWebView2;
+                initWebView2();
+                webViewControl.Source = new Uri(internalUrl);
+                webViewControl.DefaultBackgroundColor = Color.Transparent;
+                //webViewControl.NavigationCompleted += WebViewControl_NavigationCompleted;
+            }
+            else
+            {
+                if (e.InitializationException is WebView2RuntimeNotFoundException)
+                {
+                    MessageBox.Show("此计算机上没有安装WebView2运行时。\r\n访问：https://developer.microsoft.com/zh-cn/microsoft-edge/webview2/consumer/ 获取运行时", "", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    string runtimeDownloadUrl = "https://developer.microsoft.com/zh-cn/microsoft-edge/webview2/consumer/";
+                    Process p = new Process();
+                    p.StartInfo.FileName = "cmd.exe";
+                    p.StartInfo.Arguments = $"/c start {runtimeDownloadUrl}";
+                    p.Start();
+                    Application.Exit();
+                }
+                else
+                {
+                    MessageBox.Show(e.InitializationException.ToString(), "Web页面初始化失败");
+                    Application.Exit();
+                }
+            }
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            notifyIcon1.Visible = false;
+            System.Environment.Exit(0);
+        }
+
+        private void 项目地址ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Process p=new Process();
+            p.StartInfo.FileName = "cmd.exe";
+            p.StartInfo.Arguments = "/c start https://github.com/SwetyCore/MyWidget";
+            p.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (GetAsyncKeyState(Keys.RControlKey) != 0)
+            {
+                count++;
+            }
+            else
+            {
+                count = 0;
+            }
+            if (count > target)
+            {
+                this.WindowState = FormWindowState.Normal;
+                this.TopMost = true;
+                this.TopMost=false;
+                Thread.Sleep(800);
+                count = 0;
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            this.notifyIcon1.Visible = false;
+        }
+    }
+}
---
+++ MyWidget/InternalApi.cs
@@ -0,0 +1,205 @@
+﻿using Microsoft.Web.WebView2.Core;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using DGP.Genshin.GamebarWidget.MiHoYoAPI;
+using DGP.Genshin.GamebarWidget.Model;
+using DGP.Genshin.GamebarWidget.Helper;
+using Newtonsoft.Json;
+
+namespace MyWidget
+{
+    internal class InternalApi
+    {
+        ResourceHandler resourceHandler;
+        SysParams sysParams =new SysParams();
+        List<RoleAndNote> roleAndNotes = new List<RoleAndNote>();
+        int resinCount=0;
+
+        string cookiestring = "";
+        public InternalApi(ResourceHandler resourceHandler)
+        {
+            this.resourceHandler = resourceHandler;
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+            // 创建一个100ms定时的定时器
+            Timer RUtimer = new Timer(1000);    // 参数单位为ms
+                                                       // 定时时间到，处理函数为OnTimedUEvent(...)
+            RUtimer.Elapsed += OnTimedUEvent;
+            // 为true时，定时时间到会重新计时；为false则只定时一次
+            RUtimer.AutoReset = true;
+            // 使能定时器
+            RUtimer.Enabled = true;
+            // 开始计时
+            RUtimer.Start();
+
+
+        }
+        void OnTimedUEvent(object sender, ElapsedEventArgs e)
+        {
+            sysParams.getprocessorUtility();
+            sysParams.getMemAvailable();
+            sysParams.getMemCommitedPerc();
+            resinCount++;
+            //8分钟
+            if (resinCount >= 60 * 8)
+            {
+                RefreshDailyNotePoolAsync();
+                resinCount = 0;
+            }
+
+        }
+
+        private static void runcmd(string cmd)
+        {
+
+            Process p = new Process();
+            p.StartInfo.FileName = "cmd.exe";         //确定程序名
+            p.StartInfo.Arguments = "/c " + cmd;   //确定程式命令行
+            p.StartInfo.UseShellExecute = false;      //Shell的使用
+            p.StartInfo.CreateNoWindow = true;        //设置置不显示示窗口
+            p.Start();
+
+        }
+        private async Task RefreshDailyNotePoolAsync()
+        {
+            DGP.Genshin.GamebarWidget.Model.Cookie cookie = new DGP.Genshin.GamebarWidget.Model.Cookie(cookiestring);
+            List<UserGameRole> roles = await new UserGameRoleProvider(cookie.CookieValue).GetUserGameRolesAsync();
+            roleAndNotes.Clear();
+            if (roles.Count == 0)
+            {
+
+                return;
+            }
+            foreach (UserGameRole role in roles)
+            {
+                DailyNote note = await new DailyNoteProvider(cookie.CookieValue).GetDailyNoteAsync(role.Region, role.GameUid);
+                roleAndNotes.Add(new RoleAndNote { Role = role, Note = note });
+            }
+            
+
+        }
+
+        public CoreWebView2WebResourceResponse ApiReqHandler(CoreWebView2WebResourceRequest req)
+        {
+            string path = req.Uri.Substring(Form1.internalUrl.Length, req.Uri.Length - Form1.internalUrl.Length);
+
+            JObject resp = new JObject();
+            switch (req.Method)
+            {
+
+
+                case "POST":
+                    {
+
+                        Stream s = req.Content;
+                        StreamReader reader = new StreamReader(s);
+                        string text = reader.ReadToEnd();
+                        JObject o = JObject.Parse(text);
+
+                        switch (path)
+                        {
+                            case "/api/runcmd":
+                                {
+                                    string cmd = (string)o.SelectToken("cmd");
+                                    runcmd(cmd);
+                                    return resourceHandler.FromString("ok", Encoding.Default, "");
+                                }
+                                break;
+
+                            case "/api/debug":
+                                {
+                                    bool debug = (bool)o.SelectToken("debug");
+                                    string url = (string)o.SelectToken("url");
+                                    Form1.debug = debug;
+                                    if (debug)
+                                    {
+                                        Form1.internalUrl = url;
+
+                                    }
+                                    else
+                                    {
+                                        Form1.internalUrl = Form1.myhost;
+                                    }
+                                    return resourceHandler.FromString("ok", Encoding.Default, "");
+                                }
+                                break;
+                            case "/api/getAvator":
+                                {
+                                    string name = (string)o.SelectToken("name");
+
+                                    return resourceHandler.FromString(NeteaseCloudMusic.FindAvator(name), Encoding.Default, "");
+
+                                }
+                                break;
+                            case "/api/setResinCookie":
+                                {
+
+                                    string cookie = (string)o.SelectToken("cookie");
+
+                                    //cookiestring = cookie;
+                                    cookiestring = cookie;
+                                    RefreshDailyNotePoolAsync();
+                                    //RefreshDailyNotePoolAsync();
+                                    //JObject response = new JObject();
+                                    //response["ResinFormatted"] = "";
+                                    return resourceHandler.FromString("ok", Encoding.Default, "");
+                                }
+                                break;
+                        }
+                    }break;
+
+                case "GET":
+                    {
+                        switch (path)
+                        {
+
+                            case "/api/performance":
+                                {
+                                    JObject o = new JObject();
+                                    JObject mem = new JObject();
+                                    mem["avaiable"] = sysParams.MEMAvailable;
+                                    mem["commitedPrec"] = sysParams.MEMCommitedPerc;
+                                    o["cpu"] = sysParams.processorUtility;
+                                    o["mem"] = mem;
+                                    return resourceHandler.FromString(o.ToString(), Encoding.Default, "application/json");
+                                }
+                                break;
+                            case
+                                "/api/getSongName":
+                                {
+                                    return resourceHandler.FromString(NeteaseCloudMusic.FindName(), Encoding.Default, "");
+                                }
+                                break;
+                               case "/api/getResin":{
+                                    if (cookiestring=="")
+                                    {
+                                        return resourceHandler.ForErrorMessage("error", HttpStatusCode.BadRequest);
+                                    }
+                                    //RefreshDailyNotePoolAsync();
+                                    return resourceHandler.FromString(JsonConvert.SerializeObject(roleAndNotes), Encoding.Default, "application/json");
+                                }
+                                break;
+                            case "/api/debug":
+                                {
+                                    resp["debug"] = Form1.debug;
+                                    return resourceHandler.FromString(resp.ToString(), Encoding.Default, "");
+                                } break;
+                        }
+
+
+
+                    }
+                    break;
+            }
+            return resourceHandler.ForErrorMessage("error", HttpStatusCode.Forbidden);
+        }
+    }
+}
